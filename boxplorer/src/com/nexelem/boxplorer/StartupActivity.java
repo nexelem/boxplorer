f8ee05f5184c40aaeb6882c77332ec1fdf252c8c
unknown <vilecq@gmail.com>
2013-02-28 23:28:26 +0100

    fixes #32 dodanie ikony + ekran startowy

---
+++ boxplorer/src/com/nexelem/boxplorer/StartupActivity.java
@@ -0,0 +1,58 @@
+package com.nexelem.boxplorer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+
+public class StartupActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.startup);
+		getActionBar().hide();
+		
+		View logo = findViewById(R.id.logo);
+		View all = findViewById(R.id.startup);
+
+		
+		AlphaAnimation alpha = new AlphaAnimation(0, 1);
+		alpha.setInterpolator(new AccelerateInterpolator());
+		alpha.setDuration(500);
+		alpha.setFillAfter(true);
+		alpha.setStartOffset(500);
+		logo.startAnimation(alpha);
+		
+		AlphaAnimation fadeout = new AlphaAnimation(1, 0);
+		fadeout.setInterpolator(new AccelerateInterpolator());
+		fadeout.setDuration(500);
+		fadeout.setFillAfter(true);
+		fadeout.setStartOffset(2000);
+		fadeout.setAnimationListener(new AnimationListener() {
+			
+			@Override
+			public void onAnimationStart(Animation animation) {				
+			}
+			
+			@Override
+			public void onAnimationRepeat(Animation animation) {			
+			}
+			
+			@Override
+			public void onAnimationEnd(Animation animation) {
+				Intent intent = new Intent(StartupActivity.this, MainActivity.class);
+				intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				startActivity(intent);
+				finish();
+			}
+		});
+		all.startAnimation(fadeout);
+	}
+
+}
