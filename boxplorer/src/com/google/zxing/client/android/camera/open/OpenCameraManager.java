aa29bf5aae0aba53223b919ce10bdddb97ced795
darek <git@darekzon.com>
2013-03-03 22:47:09 +0100

    fixes #36 blad inicjalizacji kamery przez czytnik kodow QR gdy dostepna jest tylko kamera przednia - zmieniono obsloge kamer

--- boxplorer/src/com/google/zxing/client/android/camera/open/OpenCameraManager.java
+++ boxplorer/src/com/google/zxing/client/android/camera/open/OpenCameraManager.java
@@ -19,14 +19,13 @@ package com.google.zxing.client.android.camera.open;
 import com.google.zxing.client.android.common.PlatformSupportManager;
 
 /**
- * Selects an appropriate implementation of {@link OpenCameraInterface} based on the device's
- * API level.
+ * Selects an appropriate implementation of {@link OpenCameraInterface} based on
+ * the device's API level.
  */
 public final class OpenCameraManager extends PlatformSupportManager<OpenCameraInterface> {
 
-  public OpenCameraManager() {
-    super(OpenCameraInterface.class, new DefaultOpenCameraInterface());
-    addImplementationClass(9, "com.google.zxing.client.android.camera.open.GingerbreadOpenCameraInterface");
-  }
+	public OpenCameraManager() {
+		super(OpenCameraInterface.class, new DefaultOpenCameraInterface());
+	}
 
 }
